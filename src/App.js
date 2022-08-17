a7c5d81b2248c255a9656f4a3e9a34931a9fc781
Lydia Bell <lydiabell@Lydias-MacBook-Pro.local>
2022-08-17 05:53:42 -0500

    Removed React Router v6 and installed React Router v5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import BeerInfo from './components/BeerInfo/BeerInfo';
+import BeersList from './components/BeersList/BeersList';
+import FiltersList from './components/FiltersList/FiltersList';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import SearchBox from './components/SearchBox/SearchBox';
 
 function App() {
+
+    const [beersArray, setBeersArray] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+    const [ph, setPh] = useState(false)
+    const [abv, setAbv] = useState(false)
+    
+    useEffect(() => {
+        fetch('https://api.punkapi.com/v2/beers?per_page=80')
+        .then(response => {return response.json()})
+        .then(jsonObject => {
+            const beersObj = jsonObject;
+            setBeersArray(beersObj)
+        })
+    }, [])
+
+    const filterByPH = () => {
+        setPh(!ph)
+    }
+
+    const filterByABV = () => {
+        setAbv(!abv)
+    }
+
+    const handleInput = event => {
+        const inputValue = event.target.value.toLowerCase();
+        setSearchTerm(inputValue)
+    }
+
+    const filterResults = beersArray.filter(result => {
+        let beerHasMatched = true;
+      
+        if (searchTerm) {
+          beerHasMatched = result.name.toLowerCase().includes(searchTerm);
+        }
+      
+        if (abv) {
+          beerHasMatched = beerHasMatched && result.abv > 6;
+        }
+      
+        if (ph) {
+          beerHasMatched = beerHasMatched && result.ph < 4;
+        }
+      
+        return beerHasMatched;
+      });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+            <Nav />
+            <Header />
+            <Switch>
+                <Route path="/beerinfo/:id">
+                    {beersArray.length > 0 && <BeerInfo beersArray={beersArray}/>}
+                </Route>
+                <Route path="/">
+
+                    <SearchBox handleInput={handleInput} searchTerm={searchTerm}/>
+                    <FiltersList filterByABV={filterByABV} filterByPH={filterByPH} />
+                    <BeersList beersArray={filterResults} />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
